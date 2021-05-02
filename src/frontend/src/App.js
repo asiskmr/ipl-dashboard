4f04950942202e3b17e7b83f7a2adc4385749bfc
asiskmr <asiskmr@gmail.com>
2021-05-02 12:49:09 +0530

    React App with team page

---
+++ src/frontend/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { Teampage } from './pages/TeamPage';
+
+function App() {
+  return (
+    <div className="App">
+     <Teampage />
+    </div>
+  );
+}
+
+export default App;
--- src/main/java/com/yashtech/ipldeshboard/controller/TeamController.java
+++ src/main/java/com/yashtech/ipldeshboard/controller/TeamController.java
@@ -4,11 +4,13 @@ import com.yashtech.ipldeshboard.model.Team;
 import com.yashtech.ipldeshboard.repository.MatchRepository;
 import com.yashtech.ipldeshboard.repository.TeamRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
     
     private final TeamRepository teamRepository;
